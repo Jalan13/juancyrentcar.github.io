c285516531958b1cae0812e17d1ec716f0c8fc05
Jalan13 <jalanperezg@gmail.com>
2025-08-18 16:32:19 -0400

    Update main.js

--- main.js
+++ main.js
@@ -122,7 +122,7 @@ const vehiculos = [
     {
         id: 9,
         titulo: "Hyundai Santa Fe 2015",
-        imagen: "Imagen exterior santa fe",
+        imagen: "Imagen exterior santa fe.jpg",
         precio: 45,
         marca: "hyundai",
         caracteristicas: ["SUV", "5 Asientos", "AWD", "Navegación"],
@@ -131,7 +131,7 @@ const vehiculos = [
         combustible: "Gasolina",
         año: 2015,
         galeria: [
-            "Imagen interior santa fe",
+            "Imagen interior santa fe.jpg",
             "Video hyundai santa fe.mp4"
         ]
     }
@@ -406,6 +406,7 @@ function abrirModalGaleria(vehiculo) {
     });
 
 }
+
 
 
 
